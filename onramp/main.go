8086c73c22abc69a974431b5eaf66b97455da18b
Wayne E. Seguin <wayneeseguin@gmail.com>
2022-09-11 15:38:51 -0400

    Dev mode by default w/live code reloading. onramp app can connect to the postgres now.

--- onramp/main.go
+++ onramp/main.go
@@ -5,10 +5,8 @@ import (
   "fmt"
   "os"
   "github.com/jackc/pgx/v4"
-
   //"github.com/jackc/pgx/v4/pgxpool"
 )
-
 
 func main() {
   println("Hello, Business World!!!")
@@ -21,13 +19,13 @@ func main() {
 	defer conn.Close(ctx.Background())
 
   var timestamp string
-  stmt := "SELECT CURRENT_TIMESTAMP::string"
+  stmt := "SELECT to_char(CURRENT_TIMESTAMP, 'YYYY-mm-dd HH24:MM:SS')"
   err = conn.QueryRow(ctx.Background(), stmt).Scan(&timestamp)
   if err != nil {
     fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
     os.Exit(1)
   }
-  fmt.Fprintf(os.Stdout, "The current timestamp from pg is: %s", timestamp)
+  fmt.Fprintf(os.Stdout, "The postgres current timestamp is: %s\n", timestamp)
 }
 
 
